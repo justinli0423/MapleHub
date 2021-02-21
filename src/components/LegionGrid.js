2f3f0365ba4bd4ae796f498a01cf6b5b88e2c432
Justin Li <justin.li0423@gmail.com>
2021-02-21 13:13:49 -0500

    switched to checking for modifying positions by 25 instead of reading tiles

--- src/components/LegionGrid.js
+++ src/components/LegionGrid.js
@@ -33,47 +33,11 @@ const LegionGrid = ({ grid, connectDropTarget, droppedTile }) => {
     const prevOffsetTop = tileElement.props.position.y;
     const prevOffsetLeft = tileElement.props.position.x;
     const { offsetTop, offsetLeft } = droppedPosition;
-    const x = prevOffsetLeft + offsetLeft;
-    const y = prevOffsetTop + offsetTop;
-    const legionGridContainer = document
-      .querySelector("table#legionTableContainer")
-      .getBoundingClientRect();
-    removeOverlayTile(tileId);
-    // determine the offset from (tile) -> (grid) and then
-    // (grid) -> (document) and then we target the element
-    // that is at that position and hope it is a tablecell
-    const elAtPosition = document.elementFromPoint(
-      legionGridContainer.x + x,
-      legionGridContainer.y + y
-    );
-    if (
-      !elAtPosition ||
-      !elAtPosition.offsetParent ||
-      elAtPosition.offsetParent.nodeName !== "TD"
-    ) {
-      return;
-    }
+    // round to the closest tile position, but append 1 so that it
+    const x = Math.round((prevOffsetLeft + offsetLeft) / 25) * 25 - 1;
+    const y = Math.round((prevOffsetTop + offsetTop) / 25) * 25;
 
-    if (
-      overlayTileIds.includes(
-        elAtPosition.offsetParent.offsetParent.getAttribute("id")
-      )
-    ) {
-      // position is reading from the overlaying tiles, need to read from grid tiles
-      // instead
-      console.log("here");
-      // const overlayTile = elAtPosition.offsetParent;
-      return;
-    }
-
-    addNewOverlayTile(
-      legion,
-      {
-        x: elAtPosition.offsetParent.offsetLeft,
-        y: elAtPosition.offsetParent.offsetTop,
-      },
-      tileId
-    );
+    addNewOverlayTile(legion, { x, y }, tileId);
   };
 
   const [, dropRef] = useDrop({
